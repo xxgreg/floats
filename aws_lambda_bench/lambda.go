a24c318c5c21b59264a17c984adbef4b7cf88116
Greg Lowe <greg@v7.nz>
2022-01-07 23:03:28 +1300

    [benchmark] AddManyTo()

--- aws_lambda_bench/lambda.go
+++ aws_lambda_bench/lambda.go
@@ -47,17 +47,31 @@ func Handler(ctx context.Context, req Request) (Response, error) {
 }
 
 func Test() {
-	n := 16
-	{
-		a := make([]float64, 24)
-		b := make([]float64, 24)
-		for i := 0; i < 16; i++ {
-			a[i] = float64(i + 1)
-			b[i] = float64((i + 1) * 2)
-		}
-		f64.Add(a[:n], b[:n])
-		fmt.Println("Add", a, b)
+
+	n := 10
+	dst := make([]float64, n)
+	a := slice(1, n)
+
+	f64.Add(dst[:10], a[:10])
+
+	//dst[10] = 2
+
+	fmt.Println(dst)
+
+	//n = 24
+	//dst = make([]float64, n)
+	//a = slice(1, n)
+	//b := slice(2, n)
+	//AddTo(dst[:10], a[:10], b[:10])
+	//fmt.Println(dst)
+}
+
+func slice(x float64, n int) []float64 {
+	s := make([]float64, n)
+	for i := 0; i < n; i++ {
+		s[i] = x * float64(i)
 	}
+	return s
 }
 
 func BenchmarkAll() {
@@ -70,6 +84,7 @@ func BenchmarkAll() {
 	}{
 		{"Add", func(b *testing.B) { benchOp(b, f64.Add, size) }},
 		{"AddTo", func(b *testing.B) { benchToOp(b, f64.AddTo, size) }},
+		{"AddManyTo", func(b *testing.B) { BenchmarkAddManyTo(b, size) }},
 		{"Mul", func(b *testing.B) { benchOp(b, f64.Mul, size) }},
 		{"MulTo", func(b *testing.B) { benchToOp(b, f64.MulTo, size) }},
 		{"Add Gonum", func(b *testing.B) { benchOp(b, floats.Add, size) }},
@@ -78,13 +93,50 @@ func BenchmarkAll() {
 		{"MulTo Gonum", func(b *testing.B) { benchToOp(b, floats.MulTo, size) }},
 	}
 
-	for _, s := range []int{8, 64, 600, 1000, 10000} {
+	fmt.Println("function,asm,elements,ns/op")
+
+	for _, s := range []int{15, 600} {
 		size = s
 		for _, bm := range bms {
 			r := testing.Benchmark(bm.fn)
-			nsPerElement := fmt.Sprintf("%v ns/el", float64(r.NsPerOp())/float64(size))
-			fmt.Println(bm.name, size, r, nsPerElement)
+
+			asm := "asm"
+			if !UsingAsm {
+				asm = "noasm"
+			}
+
+			fmt.Printf("%v,%v,%v,%v\n", bm.name, asm, size, r.NsPerOp())
 		}
+	}
+}
+
+func BenchmarkAddManyTo(b *testing.B, size int) {
+	dst := make([]float64, size)
+
+	a := make([]float64, size)
+	for i := range a {
+		a[i] = float64(i) * 0.078
+	}
+
+	b2 := make([]float64, size)
+	for i := range a {
+		b2[i] = float64(i) * 0.045
+	}
+
+	c := make([]float64, size)
+	for i := range a {
+		c[i] = float64(i) * 0.067
+	}
+
+	d := make([]float64, size)
+	for i := range a {
+		d[i] = float64(i) * 0.023
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+	for i := 0; i < b.N; i++ {
+		f64.AddManyTo(dst, a, b2, c, d)
 	}
 }
 
