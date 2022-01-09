884c9e730da05092e26b2ebfabe8c7e9c1afe88d
Greg Lowe <greg@v7.nz>
2022-01-09 15:03:04 +1300

    [f64] neon ScaleTo

--- floats/f64/f64_neon.s
+++ floats/f64/f64_neon.s
@@ -35,15 +35,36 @@ Done:
     RET
 
 
-// func scale8(dst, a *float64, c float64, n int)
+// func scale8(dst, x *float64, c float64, n int)
 TEXT ·scale8(SB), NOSPLIT|NOFRAME, $0-32
 
-    //MOVQ    dst(FP), AX
-    //MOVQ    a+8(FP), BX
-    //MOVSD   c+16(FP), X0
-    //MOVQ    n+24(FP), CX
+        MOVD    dst(FP), R1
+        MOVD    x+8(FP), R2
+        MOVD    c+16(FP), R3
+        MOVD    n+24(FP), R4
 
-    RET
+        LSL     $3, R4, R5
+        ADD     R1, R5, R5            // end = dst + n*8
+
+        VDUP    R3, V4.D2             // duplicate the value c into both lanes of the 128-bit vector register
+Loop:
+
+        // Arm64 Neon SIMD uses 2xfloat64 in 128bit registers. These are unrolled 4x.
+        VLD1.P (R2), [V0.D2, V1.D2, V2.D2, V3.D2]
+
+        WORD $0x6E64DC00              // fmul v0.2d, v0.2d, v4.2d
+        WORD $0x6E64DC21              // fmul v1.2d, v1.2d, v4.2d
+        WORD $0x6E64DC42              // fmul v2.2d, v2.2d, v4.2d
+        WORD $0x6E64DC63              // fmul v3.2d, v3.2d, v4.2d
+
+        VST1.P [V0.D2, V1.D2, V2.D2, V3.D2], (R1)
+
+        CMP     R5, R1                // if dst >= end goto Done
+        BGE     Done
+        JMP     Loop
+
+Done:
+        RET
 
 
 // fma8() don't need this?
